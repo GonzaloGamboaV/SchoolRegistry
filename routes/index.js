84468e8c5a6b229167dce0b9194c30d96b76daa4
mpwv <mpwv@fresnou.org>
2022-02-11 08:56:08 -0800

    added courses

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const courseController = require('../controllers/courseController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +8,4 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+router.get('/courses/profile/:id')
